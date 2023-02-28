103fbc3e7fb191f7933cd64c900e3d9901f1881c
Miracle <Miracle_zjy@outlook.com>
2023-02-28 19:48:30 +0800

    Update title and links

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,8 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  // title: "Spencer's OneDrive",
+  title: "HUST CS 资源共享站",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -45,7 +46,8 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  // email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:jackzhang_2002@qq.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
@@ -53,7 +55,8 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      // link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/Slapaf',
     },
   ],
 
